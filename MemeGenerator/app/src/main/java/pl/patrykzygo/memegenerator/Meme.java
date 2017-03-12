78856fc9c59dd420fd3ae253e9974f593885f760
Mallorax <mallorax@gmail.com>
2017-03-12 19:38:53 +0100

    Meme class
    That class resolves object that will be put into our list.

---
+++ MemeGenerator/app/src/main/java/pl/patrykzygo/memegenerator/Meme.java
@@ -0,0 +1,31 @@
+package pl.patrykzygo.memegenerator;
+
+public class Meme {
+
+    private String name;
+    private int imageResource;
+
+    public Meme(String name, int imageResource) {
+        this.name = name;
+        this.imageResource = imageResource;
+    }
+
+
+    //getters
+    public String getName() {
+        return name;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+
+    //setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setImageResource(int imageResource) {
+        this.imageResource = imageResource;
+    }
+}
