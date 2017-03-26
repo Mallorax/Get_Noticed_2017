247617d03b20e85d13219ea3fa5b71de4bb0c903
Mallorax <mallorax@gmail.com>
2017-03-26 21:48:35 +0200

    Last setup for sprint bracnch
    I'm deleteing this because i'm gonna cover this in my next post for 'Get
    Noticed'

--- MemeGenerator/app/src/main/java/pl/patrykzygo/memegenerator/MemeListAdapter.java
+++ MemeGenerator/app/src/main/java/pl/patrykzygo/memegenerator/MemeListAdapter.java
@@ -10,20 +10,15 @@ import android.widget.TextView;
 
 import java.util.List;
 
-//TODO: comment this code so it's somehow understandable
 
 public class MemeListAdapter extends RecyclerView.Adapter<MemeListAdapter.MemeHolder>{
 
     private List<Meme> memeList;
-    private OnEntryClickListener onEntryClickListener;
 
     public MemeListAdapter (List<Meme> memeList){
         this.memeList = memeList;
     }
 
-    public void setOnEntryClickListener(OnEntryClickListener onEntryClickListener){
-        this.onEntryClickListener = onEntryClickListener;
-    }
 
     @Override
     public MemeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -44,28 +39,18 @@ public class MemeListAdapter extends RecyclerView.Adapter<MemeListAdapter.MemeHo
         return memeList.size();
     }
 
-    public interface OnEntryClickListener{
-        void onEntryClick(View view, int position, List<Meme> memeList);
-    }
 
     //ViewHolder class that provides a references to the views
-    public class MemeHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public class MemeHolder extends RecyclerView.ViewHolder{
 
         private TextView memeNameTextView;
         private ImageView memeImageView;
 
         public MemeHolder(View view){
             super(view);
-            view.setOnClickListener(this);
             memeNameTextView = (TextView) view.findViewById(R.id.meme_text_view);
             memeImageView = (ImageView) view.findViewById(R.id.meme_list_image);
         }
 
-        @Override
-        public void onClick(View v) {
-            if(onEntryClickListener != null){
-                onEntryClickListener.onEntryClick(v, getLayoutPosition(), memeList);
-            }
-        }
     }
 }
