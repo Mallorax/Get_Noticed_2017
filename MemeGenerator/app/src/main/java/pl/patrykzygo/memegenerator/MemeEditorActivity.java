5f0a25972dde6a7698ce79dc22aaf761992c17df
Mallorax <mallorax@gmail.com>
2017-04-07 20:56:19 +0200

    Last fix up for sprint#4 branch

--- MemeGenerator/app/src/main/java/pl/patrykzygo/memegenerator/MemeEditorActivity.java
+++ MemeGenerator/app/src/main/java/pl/patrykzygo/memegenerator/MemeEditorActivity.java
@@ -3,14 +3,10 @@ package pl.patrykzygo.memegenerator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MemeEditorActivity extends AppCompatActivity {
 
@@ -36,44 +32,6 @@ public class MemeEditorActivity extends AppCompatActivity {
         if(b != null){
             int imageID = b.getInt("image");
             memeImage.setImageResource(imageID);
-
-            topEditText.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    topTextView.setText(s.toString());
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {}
-            });
-
-            bottomEditText.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                }
-
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    bottomTextView.setText(s.toString());
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {
-
-                }
-            });
-
-            saveButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast toast = Toast.makeText(v.getContext(), "Does nothing yet", Toast.LENGTH_SHORT);
-                    toast.show();
-                }
-            });
         }
     }
 }
