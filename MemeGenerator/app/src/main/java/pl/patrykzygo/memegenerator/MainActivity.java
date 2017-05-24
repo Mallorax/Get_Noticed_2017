4d350388fc49ec0edd884888c104331b519950d0
Mallorax <mallorax@gmail.com>
2017-05-24 21:58:54 +0200

    Added simple toest to display when toolbar menu item is clicked

--- MemeGenerator/app/src/main/java/pl/patrykzygo/memegenerator/MainActivity.java
+++ MemeGenerator/app/src/main/java/pl/patrykzygo/memegenerator/MainActivity.java
@@ -7,7 +7,9 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import pl.patrykzygo.memegenerator.Database.DefaultMemes;
 
@@ -47,5 +49,27 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_activity_toolbar_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.from_camera:
+                fromCamera();
+                return true;
+            case R.id.from_gallery:
+                fromGallery();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void fromGallery(){
+        Toast.makeText(this, "Chosen gallery option", Toast.LENGTH_LONG).show();
+    }
+
+    public void fromCamera(){
+        Toast.makeText(this, "Chosen camera option", Toast.LENGTH_LONG).show();
     }
 }
