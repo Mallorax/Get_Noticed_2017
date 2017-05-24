b9844eb6fcdaa3ba9de310c5b8e00ab897ac9214
Mallorax <mallorax@gmail.com>
2017-05-24 22:25:55 +0200

    Extracted toolbar to diffrent layout file

---
+++ MemeGenerator/app/src/main/java/pl/patrykzygo/memegenerator/ImageHandlers/AddMemeActivity.java
@@ -0,0 +1,15 @@
+package pl.patrykzygo.memegenerator.ImageHandlers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pl.patrykzygo.memegenerator.R;
+
+public class AddMemeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_meme);
+    }
+}
